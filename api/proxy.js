f07cecdf102cc5e0077987ac69d1486f573cb35e
PedroSMG10 <shamamoriginal@gmail.com>
2026-08-19 15:13:47 -0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
 
   try {
     // 🔽 SUBSTITUA PELA URL CORRETA DO SEU WEB APP
-    const TARGET_URL = 'https://script.google.com/macros/s/SEU_ID_AQUI/exec';
+    const TARGET_URL = 'https://script.google.com/macros/s/AKfycbzK7Kxi7iSfEQiw5ZESpWQmucz5FYN-Obe1ZMlLyOzosz7IQFDLmoqIi-tpE_UT1NxQ/exec';
 
     // Constrói a URL com os parâmetros (para GET)
     const queryParams = new URLSearchParams(req.query).toString();
